2cb0be6e30017236301de632db3ab6a84bae70fa
SimasJakubenas <>
2023-10-24 22:54:55 +0000

    get 5 last numbers in each sanddwich sales column

--- run.py
+++ run.py
@@ -80,6 +80,21 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully.\n')
 
+def get_last_5_entries_sales():
+    """
+    Collect collums of data from sales worksheet.
+    Collect last 5 endries for each sandwitch.
+    return data as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
 def main():
     """
     Run all program function
@@ -92,4 +107,6 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 print('Welcome to Love Sandwiches data automation')
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
